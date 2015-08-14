1cae03dd54df999c75558f2c32bef440e9f9656e
V3507 (Conference ID) <v3507@cam.ac.uk>
2015-08-14 12:14:38 +0100

    Driving robot Yay

--- drone-backend.js
+++ drone-backend.js
@@ -11,10 +11,41 @@ Cylon.robot()
         driver: "ardrone",
         connection: "ardrone"
     })
+    .device("nav", {
+        driver: "ardrone-nav",
+connection: "ardrone"
+})
+
     .on("ready", fly);
     
 // Fly the bot
+
 function fly(robot) {
+    bot = robot;
+    bot.drone.disableEmergency();
+    bot.drone.ftrim();
+
+    bot.nav.on("navdata", function(data) {
+        console.log(data);
+    });
+
+     // Take off
+    bot.drone.takeoff();
+
+    after(9*1000, function() {
+        bot.drone.left(0.2);
+    });
+    after(12*1000, function() {
+        bot.drone.left(0);
+    });
+
+    after(15*1000,function() {
+        bot.drone.land();
+    });
+
+    after(18*1000, function() {
+        bot.drone.stop();
+    });
 
 }
 
--- drone-frontend.js
+++ drone-frontend.js
@@ -1,20 +1,42 @@
 function init() {
-    tracker = initTracker("#example");
+    var tracker = initTracker("#example");
     tracking.track("#example .drone", tracker);
 }
 
 function initTracker(element) {
     // Initialise a color tracker
     var tracker = new tracking.ColorTracker();
-    
+
+    TrackerUtils.addTrackingColor("#C44A11", "red", tracker);
+    TrackerUtils.addTrackingColor("#5CA75E", "green", tracker);
+    TrackerUtils.addTrackingColor("#AC4E43", "magenta", tracker);
+    TrackerUtils.addTrackingColor("#3E4445", "grey", tracker);
     TrackerUtils.startTrackingColors(tracker);
 
     // Whenever there is a new color detected, mark them
     tracker.on('track', function(event) {
         console.log(event.data);
-    });
+        markColors(event.data, element);
 
-    return tracker;
+        });
+
+        return tracker;
+    }
+
+function markColors(colors, element) {
+    var canvas = $(element + ' .canvas').get(0);
+    var context = canvas.getContext('2d');
+    context.clearRect(0, 0, context.width, context.height);
+
+    for (var i = 0; i < colors.length; i++) {
+        drawRectangle(colors[i], context);
+
+    }
+}
+
+function drawRectangle(rect, context) {
+    context.strokeStyle = rect.color;
+    context.strokeRect(rect.x, rect.y, rect.width, rect.height);
 }
 
 window.addEventListener("load", init);
